42f34c77dafb5462018068afa20a3ad838aa5055
trilambda122 <trilambda122>
2020-12-08 23:18:03 -0600

    setup pages for home and experince

--- src/components/ContEducation.js
+++ src/components/ContEducation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function About() {
+export default function ContEducation() {
   return (
     <div>
       <h1>ABOUT</h1>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <h1>FOOTER</h1>
+  <div className="social-icons  ml-5" >
+                    <a className="social-icon mb-3" href="https://www.linkedin.com/in/shane-schilling/"><i className="fab fa-linkedin-in"></i></a>
+                    <a className="social-icon mb-3" href="https://github.com/trilambda122"><i className="fab fa-github"></i></a>
+                    {/* <!-- <a className="social-icon" href="#"><i className="fab fa-twitter"></i></a>
+                    <a className="social-icon" href="#"><i className="fab fa-facebook-f"></i></a> --> */}
+                </div>
     </div>
   )
 }
