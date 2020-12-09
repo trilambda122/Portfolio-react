94a53a7746b197ab351df5a0114b15c2021068c2
trilambda122 <trilambda122>
2020-12-08 22:41:15 -0600

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import Home from "./components/Home"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Portfolio from "./components/Portfolio"
+import Resume from "./components/Resume"
+import Error from "./components/Error"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+      <Header/>
+  <Switch>
+    
+      <Route exact path='/'>
+        <Home/>
+      </Route>
+
+      <Route path='/about'>
+        <About/>
+      </Route>
+
+      <Route path='/resume'>
+        <Resume/>
+      </Route>
+
+      <Route path='/portfolio'>
+        <Portfolio/>
+      </Route>
+
+      <Route path="*">
+        <Error/>
+      </Route>
+
+    
+  </Switch>
+    <Footer/> 
+  </Router>
+
   );
 }
 
