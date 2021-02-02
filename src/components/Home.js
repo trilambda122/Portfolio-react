916d3bf25276fe650ea18a640557df415d3525e5
trilambda122 <trilambda122>
2021-02-02 17:49:59 -0600

    added icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,48 @@ export default function Home() {
                 </div>
                 <p className="lead mb-3">Certified Full Stack Developer who can leverage 20 years of technical and business experience.  
                   Blending in-depth business acumen with a broad understanding of the technological landscape. 
-                  Committed to continuous learning, I seek opportunities to sharpen my craft and contribute to dynamic team environments.</p>     
+                  Committed to continuous learning, I seek opportunities to sharpen my craft and contribute to dynamic team environments.</p>  
+
+
+<hr></hr>
+<p>
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/html5-original.svg" width="30" alt="html5" /> HTML5
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/nodejs-original.svg" width="30" alt="NodeJS"  /> NodeJS
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/css3-original.svg" width="30" alt="CSS" /> CSS
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/javascript-original.svg" width="30" alt="Javascript" /> Javascript
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/python-original.svg" width="30" alt="Python" /> Python
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/mysql-original.svg" width="30" alt="MySQL" /> MySql
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/postgresql-original.svg" width="30" alt="PostgreSQL" /> PostgreSQL
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/mongodb-original.svg" width="30" alt="MongoDb" /> MongoDb
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/react-original.svg" width="30" alt="React" /> React
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/bootstrap-plain.svg" width="30" alt="Bootstrap" /> Bootstrap
+</p>
+
+
+
+<p>
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/github-original.svg" width="30" alt="Github" /> Github
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/git-original.svg" width="30" alt="Git" /> Git
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/visualstudio-plain.svg" width="30" alt="VsCode" /> VsCode
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/vim-original.svg" width="30" alt="Vim" /> VIM
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/docker-original.svg" width="30" alt="Docker" /> Docker
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/ssh-original-wordmark.svg" width="30" alt="SSH" /> SSH
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/icons8-figma.svg" width="30" alt="Inkscape" /> Figma
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/gimp-original.svg" width="30" alt="Gimp" /> Gimp
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/inkscape-original.svg" width="30" alt="Inkscape" /> Inkscape
+</p>
+
+<p>
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/linux-plain.svg" width="30" alt="Linux" /> Unix / Linux
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/heroku-original.svg" width="30" alt="Heroku" /> Heroku
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/amazonwebservices-original.svg" width="30" alt="AWS" /> AWS
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/apple-original.svg" width="30" alt="MusicKit" /> MusicKit
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/icons8-dns.svg" width="30" alt="DNS" /> DNS
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/icons8-vmware.svg" width="30" alt="Hypervisors" /> Hypervisors
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/icons8-voip.svg" width="30" alt="VOIP/SIP" /> VOIP/SIP
+<img className="me-2 ms-2 img-thumbnail" src="./assets/icons/icons8-map-marker.svg" width="30" alt="GIS" /> GIS
+</p>
+   
             </div>
         </section>
     </div>
